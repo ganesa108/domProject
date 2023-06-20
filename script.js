685ba341ae570505ac93b1078ba9e26cba132607
shivani3301 <ksweedan7@icloud.com>
2023-06-20 09:42:38 +0300

    domProject

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const itemPrices = document.getElementsByClassName("itemPrice");
 const quantity = document.querySelector(".quantity");
 const itemQuantity = document.getElementsByClassName("itemQuantity");
 let quantity2 = parseInt(quantity.innerText);
-let totalPrice2 = parseInt(totalPrice.innerText);
+let totalPrice2 = parseFloat(totalPrice.innerText);
 
 function increment() {
     for (let i = 0; i < incBtns.length; i++) {
     incBtns[i].addEventListener("click", function () {
         quantity2++;
         quantity.textContent = quantity2;
-        totalPrice2 += parseInt(itemPrices[i].innerText);
-        totalPrice.textContent = totalPrice2;
+        totalPrice2 += parseFloat(itemPrices[i].innerText);
+        totalPrice.textContent = totalPrice2.toFixed(2);
         let itemQuantity2 = parseInt(itemQuantity[i].innerText);
         itemQuantity2++;
         itemQuantity[i].textContent = itemQuantity2;
@@ -44,10 +44,10 @@ function decrement() {
       totalPrice2 = 0;
       for (let j = 0; j < itemQuantity.length; j++) {
         totalPrice2 +=
-          parseInt(itemPrices[j].innerText) *
+          parseFloat(itemPrices[j].innerText) *
           parseInt(itemQuantity[j].innerText);
       }
-      totalPrice.textContent = totalPrice2;
+      totalPrice.textContent = totalPrice2.toFixed(2);
     });
   }
 }
